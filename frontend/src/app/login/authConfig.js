f76e69ebd6fc598bd347b3d7cd9faf673d041f65
cloudtestprove <110888049+cloudtestprove@users.noreply.github.com>
2023-07-18 11:28:35 +0000

    Add files via upload

---
+++ frontend/src/app/login/authConfig.js
@@ -0,0 +1,26 @@
+ 
+// Config object to be passed to Msal on creation.
+// For a full list of msal.js configuration parameters, 
+// visit https://azuread.github.io/microsoft-authentication-library-for-js/docs/msal/modules/_authenticationparameters_.html
+const msalConfig = {
+  auth: {
+    clientId: "61f8ac6f-62be-4f20-b70a-46410b7df270",
+    authority: "https://login.microsoftonline.com/a1a7c9c1-a8e4-454a-b0cd-d0428de5014c",
+    redirectUri: "https://40.112.53.92:443",
+  },
+  cache: {
+    cacheLocation: "sessionStorage", // This configures where your cache will be stored
+    storeAuthStateInCookie: false, // Set this to "true" if you are having issues on IE11 or Edge
+  }
+};  
+  
+// Add here the scopes to request when obtaining an access token for MS Graph API
+// for more, visit https://github.com/AzureAD/microsoft-authentication-library-for-js/blob/dev/lib/msal-core/docs/scopes.md
+const loginRequest = {
+  scopes: ["openid", "profile", "User.Read"]
+};
+
+// Add here scopes for access token to be used at MS Graph API endpoints.
+const tokenRequest = {
+  scopes: ["Mail.Read", "User.Read"]
+};
